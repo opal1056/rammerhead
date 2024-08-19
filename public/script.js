92e82c987988e6f257b672365e918e0e3f730a7a
opal1056 <vishwa.cool05@gmail.com>
2024-08-18 21:05:29 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@
     const mod = (n, m) => ((n % m) + m) % m;
     const baseDictionary = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~-';
     const shuffledIndicator = '_rhs';
+    
     const generateDictionary = function () {
         let str = '';
         const split = baseDictionary.split('');
@@ -10,10 +11,12 @@
         }
         return str;
     };
+
     class StrShuffler {
         constructor(dictionary = generateDictionary()) {
             this.dictionary = dictionary;
         }
+
         shuffle(str) {
             if (str.startsWith(shuffledIndicator)) {
                 return str;
@@ -34,6 +37,7 @@
             }
             return shuffledIndicator + shuffledStr;
         }
+
         unshuffle(str) {
             if (!str.startsWith(shuffledIndicator)) {
                 return str;
@@ -69,14 +73,15 @@
             element.textContent = '';
         }
     }
+
     function getPassword() {
         var element = document.getElementById('session-password');
         return element ? element.value : '';
     }
+
     function get(url, callback, shush = false) {
         var pwd = getPassword();
         if (pwd) {
-            // really cheap way of adding a query parameter
             if (url.includes('?')) {
                 url += '&pwd=' + pwd;
             } else {
@@ -95,10 +100,7 @@
             if (request.status === 200) {
                 callback(request.responseText);
             } else {
-                if (!shush)
-                    setError(
-                        'unexpected server response to not match "200". Server says "' + request.responseText + '"'
-                    );
+                if (!shush) setError('unexpected server response to not match "200". Server says "' + request.responseText + '"');
             }
         };
     }
@@ -111,8 +113,7 @@
             get('/newsession', callback);
         },
         editsession(id, httpProxy, enableShuffling, callback) {
-            get(
-                '/editsession?id=' +
+            get('/editsession?id=' +
                 encodeURIComponent(id) +
                 (httpProxy ? '&httpProxy=' + encodeURIComponent(httpProxy) : '') +
                 '&enableShuffling=' + (enableShuffling ? '1' : '0'),
@@ -133,189 +134,5 @@
             api.sessionexists(id, function (exists) {
                 if (exists) {
                     get('/deletesession?id=' + id, function (res) {
-                        if (res !== 'Success' && res !== 'not found')
-                            return setError('unexpected response from server. received ' + res);
-                        callback();
-                    });
-                } else {
-                    callback();
-                }
-            });
-        },
-        shuffleDict(id, callback) {
-            get('/api/shuffleDict?id=' + encodeURIComponent(id), function (res) {
-                callback(JSON.parse(res));
-            });
-        }
-    };
+                        if (res !==
 
-    var localStorageKey = 'rammerhead_sessionids';
-    var localStorageKeyDefault = 'rammerhead_default_sessionid';
-    var sessionIdsStore = {
-        get() {
-            var rawData = localStorage.getItem(localStorageKey);
-            if (!rawData) return [];
-            try {
-                var data = JSON.parse(rawData);
-                if (!Array.isArray(data)) throw 'getout';
-                return data;
-            } catch (e) {
-                return [];
-            }
-        },
-        set(data) {
-            if (!data || !Array.isArray(data)) throw new TypeError('must be array');
-            localStorage.setItem(localStorageKey, JSON.stringify(data));
-        },
-        getDefault() {
-            var sessionId = localStorage.getItem(localStorageKeyDefault);
-            if (sessionId) {
-                var data = sessionIdsStore.get();
-                data.filter(function (e) {
-                    return e.id === sessionId;
-                });
-                if (data.length) return data[0];
-            }
-            return null;
-        },
-        setDefault(id) {
-            localStorage.setItem(localStorageKeyDefault, id);
-        }
-    };
-
-    function renderSessionTable(data) {
-        var tbody = document.querySelector('tbody');
-        while (tbody.firstChild && !tbody.firstChild.remove());
-        for (var i = 0; i < data.length; i++) {
-            var tr = document.createElement('tr');
-            appendIntoTr(data[i].id);
-            appendIntoTr(data[i].createdOn);
-
-            var fillInBtn = document.createElement('button');
-            fillInBtn.textContent = 'Fill in existing session ID';
-            fillInBtn.className = 'btn btn-outline-primary';
-            fillInBtn.onclick = index(i, function (idx) {
-                setError();
-                sessionIdsStore.setDefault(data[idx].id);
-                loadSettings(data[idx]);
-            });
-            appendIntoTr(fillInBtn);
-
-            var deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'Delete';
-            deleteBtn.className = 'btn btn-outline-danger';
-            deleteBtn.onclick = index(i, function (idx) {
-                setError();
-                api.deletesession(data[idx].id, function () {
-                    data.splice(idx, 1)[0];
-                    sessionIdsStore.set(data);
-                    renderSessionTable(data);
-                });
-            });
-            appendIntoTr(deleteBtn);
-
-            tbody.appendChild(tr);
-        }
-        function appendIntoTr(stuff) {
-            var td = document.createElement('td');
-            if (typeof stuff === 'object') {
-                td.appendChild(stuff);
-            } else {
-                td.textContent = stuff;
-            }
-            tr.appendChild(td);
-        }
-        function index(i, func) {
-            return func.bind(null, i);
-        }
-    }
-    function loadSettings(session) {
-        document.getElementById('session-id').value = session.id;
-        document.getElementById('session-httpproxy').value = session.httpproxy || '';
-        document.getElementById('session-shuffling').checked = typeof session.enableShuffling === 'boolean' ? session.enableShuffling : true;
-    }
-    function loadSessions() {
-        var sessions = sessionIdsStore.get();
-        var defaultSession = sessionIdsStore.getDefault();
-        if (defaultSession) loadSettings(defaultSession);
-        renderSessionTable(sessions);
-    }
-    function addSession(id) {
-        var data = sessionIdsStore.get();
-        data.unshift({ id: id, createdOn: new Date().toLocaleString() });
-        sessionIdsStore.set(data);
-        renderSessionTable(data);
-    }
-    function editSession(id, httpproxy, enableShuffling) {
-        var data = sessionIdsStore.get();
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].id === id) {
-                data[i].httpproxy = httpproxy;
-                data[i].enableShuffling = enableShuffling;
-                sessionIdsStore.set(data);
-                return;
-            }
-        }
-        throw new TypeError('cannot find ' + id);
-    }
-
-    get('/mainport', function (data) {
-        var defaultPort = window.location.protocol === 'https:' ? 443 : 80;
-        var currentPort = window.location.port || defaultPort;
-        var mainPort = data || defaultPort;
-        if (currentPort != mainPort) window.location.port = mainPort;
-    });
-
-    api.needpassword(doNeed => {
-        if (doNeed) {
-            document.getElementById('password-wrapper').style.display = '';
-        }
-    });
-    window.addEventListener('load', function () {
-        loadSessions();
-
-        var showingAdvancedOptions = false;
-        document.getElementById('session-advanced-toggle').onclick = function () {
-            // eslint-disable-next-line no-cond-assign
-            document.getElementById('session-advanced-container').style.display = (showingAdvancedOptions =
-                !showingAdvancedOptions)
-                ? 'block'
-                : 'none';
-        };
-
-        document.getElementById('session-create-btn').onclick = function () {
-            setError();
-            api.newsession(function (id) {
-                addSession(id);
-                document.getElementById('session-id').value = id;
-                document.getElementById('session-httpproxy').value = '';
-            });
-        };
-        function go() {
-            setError();
-            var id = document.getElementById('session-id').value;
-            var httpproxy = document.getElementById('session-httpproxy').value;
-            var enableShuffling = document.getElementById('session-shuffling').checked;
-            var url = document.getElementById('session-url').value || 'https://www.google.com/';
-            if (!id) return setError('must generate a session id first');
-            api.sessionexists(id, function (value) {
-                if (!value) return setError('session does not exist. try deleting or generating a new session');
-                api.editsession(id, httpproxy, enableShuffling, function () {
-                    editSession(id, httpproxy, enableShuffling);
-                    api.shuffleDict(id, function (shuffleDict) {
-                        if (!shuffleDict) {
-                            window.location.href = '/' + id + '/' + url;
-                        } else {
-                            var shuffler = new StrShuffler(shuffleDict);
-                            window.location.href = '/' + id + '/' + shuffler.shuffle(url);
-                        }
-                    });
-                });
-            });
-        }
-        document.getElementById('session-go').onclick = go;
-        document.getElementById('session-url').onkeydown = function (event) {
-            if (event.key === 'Enter') go();
-        };
-    });
-})();
